c609e0d7bef69ab70ecf6e89adafde47f6e41009
unknown <prasannavenkat1998@yahoo.com>
2022-12-30 00:43:57 +0530

    Added cors option

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ const PORT = process.env.PORT || 4000;
 // parse application/json
 app.use(bodyParser.json())
 //cors
-app.use(cors({ origin: "*"}))
+const corsOptions = {
+    origin: '*',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  };
+  
+  app.use(cors(corsOptions)); // Use this after the variable declaration
 
 
 
